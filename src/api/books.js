c700e11f442425f7b14dd53cd1eb8104b369a966
Artem Vershinin <art.vershinin@playa.ru>
2018-05-03 11:15:54 +0800

    #6 главная страница - контент. подключил АПИ

--- src/api/books.js
+++ src/api/books.js
@@ -4,18 +4,18 @@ import Contur from "../constants/contur";
 export default {
   new: (params) => {
     const ENDPOINT = Contur.get().API;
-    return axios.get(ENDPOINT + "/api/books");
+    return axios.get(ENDPOINT + "/api/books", { params: params });
   },
     trade: (params) => {
         const ENDPOINT = Contur.get().API;
-        return axios.get(ENDPOINT + "/api/books");
+        return axios.get(ENDPOINT + "/api/books", { params: params });
     },
     good: (params) => {
         const ENDPOINT = Contur.get().API;
-        return axios.get(ENDPOINT + "/api/books");
+        return axios.get(ENDPOINT + "/api/books", { params: params });
     },
     search: (params) => {
         const ENDPOINT = Contur.get().API;
-        return axios.get(ENDPOINT + "/api/books");
+        return axios.get(ENDPOINT + "/api/books", { params: params });
     },
 };
