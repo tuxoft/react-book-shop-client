72791324e6a80357f3f2fcd9c56a41cea87b1236
Artem Vershinin <art.vershinin@playa.ru>
2018-05-04 18:40:11 +0800

    #11 Страница поиска книг

--- src/routes/HeaderRoutes/index.js
+++ src/routes/HeaderRoutes/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Switch, Redirect, Route, withRouter } from "react-router-dom";
-import Header from "../../components/header";
+import Header from "../../containers/Header";
 
 
 const HeaderRoutes = (props) =>
