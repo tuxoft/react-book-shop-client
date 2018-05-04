3c6c5d890547f2261940f97d0268466d429c05f7
a.dikikh <a.dikikh@playa.ru>
2018-05-04 18:28:10 +0800

    #6 исправление роутов

--- src/routes/MainRoutes/index.js
+++ src/routes/MainRoutes/index.js
@@ -2,50 +2,33 @@ import React from "react";
 import { connect } from "react-redux";
 import { Switch, Redirect, Route, withRouter } from "react-router-dom";
 import MainScreen from "../../Screens/MainScreen/index";
-import SearchScreen from "../Screens/SearchScreen/index";
+import SearchScreen from "../../Screens/SearchScreen/index";
 
-const ProtectedRoute = ({ component: Component, render, ...restProps }) => {
-    return (
-        <Route
-            {...restProps}
-            render={(props) => {
-                return restProps.isAuthenticated ? (
-                    Component ? (
-                        <Component {...props} />
-                    ) : (
-                        render(props)
-                    )
-                ) : (
-                    <Redirect to="/login" />
-                );
-            }}
-        />
-    );
-};
+
 
 const Routes = (props) =>
 
   props.isInitialized ? (
     <Switch>
-        <ProtectedRoute
+        <Route
             exact
             path="/"
             render={(props) => <Redirect to="/home" {...props} />}
             {...props}
         />
-        <ProtectedRoute
+        <Route
             exact
             path="/home"
             render={(props) => <MainScreen {...props} />}
             {...props}
         />
-        <ProtectedRoute
+        <Route
             exact
             path="/search"
             render={(props) => <SearchScreen {...props} />}
             {...props}
         />
-        <ProtectedRoute
+        <Route
             exact
             path="/search/:query"
             render={(props) => <SearchScreen {...props} />}
