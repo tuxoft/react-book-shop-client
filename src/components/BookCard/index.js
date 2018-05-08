4d3f16faad835dbe4c3c79729c4ef9ecc6c0e43b
Artem Vershinin <art.vershinin@playa.ru>
2018-05-08 15:11:47 +0800

    #12 Сверстать страницу карточки книги

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -6,12 +6,12 @@ import InBoxButton from "../../containers/InBoxButton";
 const BookCard = ({book}) => (
     <styles.CardWrapper>
 
-        <styles.ImageLink to={"/books/" + book.id}>
+        <styles.ImageLink to={"/book/" + book.id}>
             <styles.Image src={book.url}/>
         </styles.ImageLink>
 
         <styles.Description>
-            <styles.TextLink to={"/books/" + book.id}>{book.name}</styles.TextLink>
+            <styles.TextLink to={"/book/" + book.id}>{book.name}</styles.TextLink>
             <styles.AutorLabel>
                 {book.autor}
             </styles.AutorLabel>
